fa93a56f7b2e32d8c0f4d8fc19fd5a0c7cd14287
eric_tu <ceparadise168@gmail.com>
2020-07-08 06:35:14 +0800

    [Feature] 新增購物車基本方法
    - 增加商品
    - 取得所有商品
    - 計算總價
    - 計算總數量

--- packges/ceparadise168/cart/src/Cart.php
+++ packges/ceparadise168/cart/src/Cart.php
@@ -4,8 +4,47 @@ namespace Ceparadise168\Cart;
 
 class Cart
 {
+    private $items = [];
     public function result()
     {
         return 'test';
+    }
+
+    public function putItem($item = [])
+    {
+        $this->items[] = $item;
+
+        return $this;
+    }
+
+    public function getItems()
+    {
+        return $this->items;
+    }
+
+    public function total()
+    {
+        $items = $this->getItems();
+
+        $total = 0;
+
+        foreach ($items as $item) {
+            $total += $item['price'] * $item['qty'];
+        }
+
+        return $total;
+    }
+
+    public function getTotalQuantity()
+    {
+        $items = $this->getItems();
+
+        $totalQuantity = 0;
+
+        foreach ($items as $item) {
+            $totalQuantity += $item['qty'];
+        }
+
+        return $totalQuantity;
     }
 }
--- packges/ceparadise168/cart/tests/CartTest.php
+++ packges/ceparadise168/cart/tests/CartTest.php
@@ -1,6 +1,7 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Ceparadise168\Cart\Cart;
 
 class CartTest extends TestCase
 {
@@ -9,5 +10,60 @@ class CartTest extends TestCase
         $string = 'hello';
 
         $this->assertEquals('hello', $string);
+    }
+
+    public function testPutItemToCartAndAssertCartItem()
+    {
+        $cart = new Cart();
+        $item = [
+            'name' => 'pen',
+            'price' => 100,
+            'qty' => 1
+        ];
+        $cart->putItem($item);
+
+        $this->assertEquals([], array_diff_assoc($item, $cart->getItems()[0]));
+    }
+
+    public function testGetCartTotal()
+    {
+        $cart = new Cart();
+
+        $item = [
+            'name' => 'pen',
+            'price' => 100,
+            'qty' => 1
+        ];
+        $cart->putItem($item);
+
+        $item = [
+            'name' => 'eraser',
+            'price' => 200,
+            'qty' => 2
+        ];
+        $cart->putItem($item);
+
+        $this->assertEquals(500, $cart->total());
+    }
+
+    public function testTotalQuantityShouldGetTotalQuantityOfCartItems()
+    {
+        $cart = new Cart();
+
+        $item = [
+            'name' => 'pen',
+            'price' => 100,
+            'qty' => 1
+        ];
+        $cart->putItem($item);
+
+        $item = [
+            'name' => 'eraser',
+            'price' => 200,
+            'qty' => 2
+        ];
+        $cart->putItem($item);
+
+        $this->assertEquals(3, $cart->getTotalQuantity());
     }
 }
